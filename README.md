5cce982172d1eaa71143d6291b49e129ad399dca
zhan <zhanyanan1992@126.com>
2020-02-21 20:58:28 +0800

    修改web、exec执行脚本 添加单机部署修改方案

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Ambari 集成 Azkaban
 
 # Major Project Structure
 - configuration : azkaban 配置文件
+- bin : Azkaban脚本修改（单机部署web、exec需要更换） 
 - package : 
   - scripts :  ambari 管理逻辑脚本
     - azkaban_executor.py  
@@ -10,5 +11,12 @@ Ambari 集成 Azkaban
     - common.py
     - download.ini
     - params.py
+
+# Dealoy
+
+- 1台服务器同时安装web、executor（需要修改azkaban的脚本，同时启动有冲突）
+- web、executor分别部署在多台服务器（无需关心脚本）
+
 # Usage
+
 https://cwiki.apache.org/confluence/display/AMBARI/Overview
--- package/scripts/azkaban_executor.py
+++ package/scripts/azkaban_executor.py
@@ -13,7 +13,8 @@
 # limitations under the License.
 
 import os.path as path
-
+import time
+import socket
 from common import AZKABAN_EXECUTOR_URL, AZKABAN_NAME, AZKABAN_HOME, AZKABAN_CONF
 from resource_management.core.exceptions import ExecutionFailed, ComponentIsNotRunning
 from resource_management.core.resources.system import Execute
@@ -22,7 +23,8 @@ from resource_management.libraries.script.script import Script
 
 class ExecutorServer(Script):
     def install(self, env):
-        from params import java_home
+        #from params import java_home
+        java_home='/usr/local/jdk1.8.0_171'
         Execute('wget --no-check-certificate {0}  -O /tmp/{1}'.format(AZKABAN_EXECUTOR_URL, AZKABAN_NAME))
         Execute(
             'mkdir -p {0} {1} {2} || echo "whatever"'.format(
@@ -42,18 +44,27 @@ class ExecutorServer(Script):
         self.configure(env)
 
     def stop(self, env):
-        Execute('cd {0} && bin/azkaban-executor-shutdown.sh'.format(AZKABAN_HOME))
+    self.configure(env)
+        Execute('cd {0} && bin/shutdown-exec.sh'.format(AZKABAN_HOME))
 
     def start(self, env):
         from params import azkaban_executor_properties
         self.configure(env)
-        Execute('cd {0} && bin/azkaban-executor-start.sh'.format(AZKABAN_HOME))
-        Execute(
-            'curl http://localhost:{0}/executor?action=activate'.format(azkaban_executor_properties['executor.port'])
-        )
+        executor_port = int(azkaban_executor_properties['executor.port']) 
+        Execute('cd {0} && bin/start-exec.sh'.format(AZKABAN_HOME))
+        while 1:
+           sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+           result = sock.connect_ex(('127.0.0.1',executor_port))
+           sock.close()
+       if result == 0:
+          Execute('curl http://localhost:{0}/executor?action=activate'.format(executor_port))
+          break
+       else:
+         time.sleep(5) 
 
     def status(self, env):
         try:
+        self.configure(env)
             Execute(
                 'export AZ_CNT=`ps -ef |grep -v grep |grep azkaban-exec-server | wc -l` && `if [ $AZ_CNT -ne 0 ];then exit 0;else exit 3;fi `'
             )
@@ -73,10 +84,14 @@ class ExecutorServer(Script):
             for key, value in azkaban_executor_properties.iteritems():
                 if key != 'content':
                     f.write(key_val_template.format(key, value))
-            f.write(azkaban_executor_properties['content'])
+            #f.write(azkaban_executor_properties['content'])
+            if azkaban_executor_properties.has_key('content'):
+                f.write(str(azkaban_executor_properties['content']))
 
         with open(path.join(AZKABAN_CONF, 'log4j.properties'), 'w') as f:
-            f.write(log4j_properties['content'])
+            #f.write(log4j_properties['content'])
+            if log4j_properties.has_key('content'):
+            f.write(str(log4j_properties['content']))
 
 
 if __name__ == '__main__':
